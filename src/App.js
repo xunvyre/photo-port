3b2024ae86012736723e141e6239b11c1ce84d6c
xunvyre <victoria@thedavids.us>
2022-10-24 17:49:28 -0400

    nav and about started and fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import './App.css';
+import React from 'react';
+import About from './components/About';
+import Nav from './components/Nav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App()
+{
+    return(
+        <div>
+            <Nav></Nav>
+            <main>
+                <About></About>
+            </main>
+        </div>
+    );
 }
 
 export default App;
